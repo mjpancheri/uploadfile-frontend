accdda632b263939195e47a92a56ab45a36b006f
Marcio Pancheri <mjpancheri@claretiano.edu.br>
2020-06-09 23:11:55 -0300

    Adicionando o projeto no git

---
+++ src/components/FileList/styles.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Container = styled.ul`
+  margin-top: 20px;
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #444;
+  
+    & + li {
+      margin-top: 15px;
+    }
+  }
+`;
+
+export const FileInfo = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      font-size: 12px;
+      color: #999;
+      margin-top: 5px;
+
+      button {
+        border: 0;
+        background: transparent;
+        color: #e57878;
+        margin-left: 5px;
+        cursor: pointer;
+      }
+    }
+
+    small {
+      font-size: 10px;
+      color: #999;
+      margin-top: 5px;
+    }
+  }
+`;
+
+export const Preview = styled.div`
+  width: 48px;
+  height: 48px;
+  border-radius: 5px;
+  background-image: url(${props => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+  margin-right: 10px;
+`;
